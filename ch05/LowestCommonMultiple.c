7f06d64611bacaa137971bd6b4874339c83028a2
Gurkan Yilmaz <gurkandy@gmail.com>
2021-03-22 00:42:07 +0300

    Lowest Common Multiple exercise completed.

--- ch05/LowestCommonMultiple.c
+++ ch05/LowestCommonMultiple.c
@@ -3,31 +3,46 @@
 
 #include <stdio.h>
 
-int lcm(int number_a, int number_b, int divider, int minimum);
+int GCF(int number_a, int number_b, int divider, int minimum);
 
 int main(void)
 {
-	int min_, divider_;
-}
+	int min_ = 1; // required in finding the greatest common factor.
+	int divider_, numberA, numberB, gcf, lcm;
 
-int lcm(int number_a, int number_b, int divider, int minimum)
-{
-	if (divider == 1)
-	{
-		printf("The Lowest Common Multiple: %d \n", minimum);
-		return 0;
+	printf("Enter Two integers (separated by \",\"): ");
+	scanf("%d, %d", &numberA, &numberB);
+
+	if (numberA > numberB){ // take the smaller number as the divider
+		divider_ = numberB;
 	}
 	else
 	{
-		if((number_a % divider == 0)&&(number_b % divider == 0))
+		divider_ = numberA;
+	}
+
+	gcf = GCF(numberA, numberB, divider_, min_);
+	lcm = (numberA * numberB) / gcf;
+	printf("The lowest common multiple of (%d,%d) is %d: \n", numberA, numberB, lcm);
+}
+
+// First find Greatest Common Factor then use it to find Lowest Common Multiple.
+int GCF(int number_a, int number_b, int divider, int minimum)
+{
+	if (divider == 1)
+	{
+		return minimum;
+	}
+	else
+	{
+		if((number_a % divider == 0)&&(number_b % divider == 0)) // Common factor
 		{
 			minimum = divider;
-			return lcm(number_a, number_b, divider--, minimum);
+			return minimum;
 		}
 		else
 		{
-
+			return GCF(number_a, number_b, divider-1, minimum);
 		}
 	}
-	
 }
