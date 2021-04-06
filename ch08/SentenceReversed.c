cb43fe85200b64c895da7ab8bbd828f46a78953e
Gurkan Yilmaz <gurkandy@gmail.com>
2021-04-06 20:23:33 +0300

    deleted unnecessary lines

--- ch08/SentenceReversed.c
+++ ch08/SentenceReversed.c
@@ -4,31 +4,23 @@
 #include <stdio.h>
 #include <string.h>
 
-//void reverse(char *, int array_size);
-
 char *reverse_sentence(char input_array[], unsigned int array_length);
 
 int main()
 {
 	char input[] = "This is an example text to tokenize.";
-	char *inputPtr = input;
 	char *tokenPtr;
 
-	printf("%lu\n", strlen(input));
-	printf("Size: %lu --%lu \t %p---%c\n", sizeof(inputPtr), sizeof(char), inputPtr, *inputPtr);
-	inputPtr++;
-	printf("inputPtr: %p -- %c \n", inputPtr, *inputPtr);
-	//printf("Token: %s\n", strtok(inputPtr, " "));
-	
+	puts("Enter a text to see it reversed: ");
+	fgets(input, 80, stdin);
+
 	tokenPtr = reverse_sentence(input, strlen(input));
-	printf("tokenPtr: %p -- %c \n", tokenPtr, *tokenPtr);
-	printf("input: %p -- %c \n", input, *input);
+//	printf("tokenPtr: %p -- %c \n", tokenPtr, *tokenPtr);
 }
 
 char *reverse_sentence(char input_array[], unsigned int  array_length)
 {
 	input_array += array_length;
-//	printf("Last char: %c \n", *input_array);
 	puts("Reversed text: ");
 	for(size_t i = 0; i < array_length; ++i)
 	{
@@ -36,7 +28,6 @@ char *reverse_sentence(char input_array[], unsigned int  array_length)
 		printf("%c", *input_array);
 	}
 	puts("");
-	//input_array += 2;
 	return input_array;
 }
 
