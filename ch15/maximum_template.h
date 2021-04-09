10b418f2cd3bb5eaaee88de3baa873a89cca51fb
Gurkan Yilmaz <gurkandy@gmail.com>
2021-04-10 00:18:16 +0300

    header for FunctionTemplate.cpp file

---
+++ ch15/maximum_template.h
@@ -0,0 +1,19 @@
+// A function template to be used in FunctionTemplate.cpp
+
+template < class T >
+T maximum(T value1, T value2, T value3)
+{
+	T maximumValue = value1;
+
+	if (value2 > maximumValue)
+	{
+		maximumValue = value2;
+	}
+
+	if (value3 > maximumValue)
+	{
+		maximumValue = value3;
+	}
+
+	return maximumValue;
+}
