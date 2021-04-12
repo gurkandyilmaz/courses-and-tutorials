c6b1ecef5697d08e11b88ab9d5c9c00055d7b34a
Gurkan Yilmaz <gurkandy@gmail.com>
2021-04-13 00:40:23 +0300

    simple species class representing endangered animals

---
+++ ch16/Species.h
@@ -0,0 +1,27 @@
+// Species class definition. This file contains the prototypes
+// to be used in the file Species.cpp which holds the implementation details.
+// A class that holds the name, country, population, growth rate for endangered species
+
+#include <string>
+
+class Species
+{
+	public:
+		explicit Species(std::string name, std::string country, int population, double rate);
+		int CalculatePopulation(int year);
+		void setName(std::string name);
+		void setCountry(std::string country);
+		void setPopulation(int population);
+		void setGrowthRate(double rate);
+		std::string getName();
+		std::string getCountry();
+		int getPopulation();
+		double getGrowthRate();
+	private:
+		std::string animalName;
+		std::string animalCountry;
+		int animalPopulation;
+		double animalGrowthRate;
+
+};
+
