af0ddf49451ea6d4f56d213a4c090003d939f0a7
Gurkan Yilmaz <gurkandy@gmail.com>
2021-04-29 00:07:55 +0300

    multiple data could be taken from the user. pointers used

--- Data Structures And Algorithm Analysis/ch00/Struct.cpp
+++ Data Structures And Algorithm Analysis/ch00/Struct.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <sstream> // required for converting strings to numerical values.
+#include <sstream> // will be used for converting strings to numerical values.
 
 struct product_type {
 	int id;
@@ -12,43 +12,56 @@ struct product_type {
 	std::string description;
 };
 
-void printProducts(product_type products);
+product_type * takeProducts(int size);
+void printProducts(product_type products[], int size);
 
 int main()
 {
 	int size;
+	std::string size_str;
+	product_type * prod_ptr = nullptr;
+	
+	std::cout << "Enter the size for the product list: ";
+	std::getline(std::cin, size_str);
+	std::stringstream(size_str) >> size;
+	
+	prod_ptr = takeProducts(size);	
+	printProducts(prod_ptr, size);
+	delete[] prod_ptr;
+}
+
+product_type * takeProducts(int size)
+{
+	product_type * products = new product_type [size]; // to be able to return a pointer dynamically allocate a block of memory
 	std::string id, price; // helper variables converting input string into numbers.
 
-	product_type electronics;
-	product_type * el_ptr = &electronics;
+	for(int i = 0; i < size; ++i)
+	{
+		std::cout << "Enter the id for the product ("<< i+1 << "): ";
+		std::getline(std::cin, id);
+		std::stringstream(id) >> (products + i)->id;
+		
+		std::cout << "Enter the name for the product ("<< i+1 << "): ";
+		std::getline(std::cin, (products + i)->name);
 	
-	std::cout << "Enter the id for the product: ";
-	std::getline(std::cin, id);
-	//std::stringstream(id) >> electronics.id; // same as below
-	std::stringstream(id) >> el_ptr->id;
+		std::cout << "Enter a description for the product (" << i+1 << "): ";
+		std::getline(std::cin, (products + i)->description);
 	
-	std::cout << "Enter the name for the product: ";
-	//std::getline(std::cin, electronics.name); // same as below
-	std::getline(std::cin, el_ptr->name);
-
-	std::cout << "Enter a description for the item: ";
-	//std::getline(std::cin, electronics.description); // same as below
-	std::getline(std::cin, el_ptr->description);
-
-	std::cout << "Enter the price for the item: ";
-	std::getline(std::cin, price);
-	//std::stringstream(price) >> electronics.price; // same as below
-	std::stringstream(price) >> el_ptr->price;
-
-	printProducts(electronics);
-
+		std::cout << "Enter the price for the product (" << i+1 << "): ";
+		std::getline(std::cin, price);
+		std::stringstream(price) >> (products + i)->price;
+	}
+	
+	return products;
 }
 
-void printProducts(product_type items)
+void printProducts(product_type products[], int size)
 {
-	std::cout << "----------PRODUCTS----------" << std::endl;
-	std::cout << "ID: " << items.id << "\nName: " << items.name << "\n";
-	std::cout << "Description: " << items.description << "\nPrice: " << items.price << "\n";
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << "----------PRODUCTS----------" << std::endl;
+		std::cout << "ID: " << products[i].id << "\nName: " << products[i].name << "\n";
+		std::cout << "Description: " << products[i].description << "\nPrice: " << products[i].price << "\n";
+		std::cout << "---------------------------\n";
+	}
 }
-
-
