bcb854a61cb6cbecbadd32dccf00ed023c522e1c
Gurkan Yilmaz <gurkandy@gmail.com>
2021-04-28 20:24:09 +0300

    removed unnecessary increments, clear variable names

--- Data Structures And Algorithm Analysis/ch00/DynamicMemory.cpp
+++ Data Structures And Algorithm Analysis/ch00/DynamicMemory.cpp
@@ -7,15 +7,15 @@
 int main()
 {
 	int size, counter;
-	int * pointer;
+	int * user_array;
 
 	std::cout << "Enter the size of the array: ";
 	std::cin >> size;
 	
 	//allocate a block of memory
-	pointer = new (std::nothrow) int[size]; // nothrow will assign nullptr to pointer if there is a failure
+	user_array = new (std::nothrow) int[size]; // nothrow will assign nullptr to user_array if there is a failure
 	
-	if (pointer == nullptr)
+	if (user_array == nullptr)
 	{
 		std::cout << "Memory could not be allocated!\n";
 	}
@@ -24,21 +24,19 @@ int main()
 		for(counter = 0; counter < size; ++counter)
 		{
 			std::cout << "Enter the value at the index " << counter+1 << ": ";
-			std::cin >> *(pointer + counter);
-			++counter;
+			std::cin >> *(user_array + counter);
 		}
 		
 		std::cout << "Array: [ ";
 		for(counter = 0; counter < size; ++counter)
 		{
-			std::cout <<  *(pointer + counter) << " ";
-			++counter;
+			std::cout <<  *(user_array + counter) << " ";
 		}
 		std::cout << "]\n";
 	}
 	
 	// release the memory allocated by the array
-	delete[] pointer;
+	delete[] user_array;
 	
 	return 0;
 }
