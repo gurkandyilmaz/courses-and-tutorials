32e381df360f82d12a8636b55210f234d42b6fab
Gurkan Yilmaz <gurkandy@gmail.com>
2021-05-01 23:42:42 +0300

    overloding + operator

--- Data Structures And Algorithm Analysis/ch00/ClassesIntro.cpp
+++ Data Structures And Algorithm Analysis/ch00/ClassesIntro.cpp
@@ -7,11 +7,23 @@ class Rectangle {
 	public:
 		Rectangle(): width {1}, height {1}{} // default constructor sets width and height to 1.
 		Rectangle(int w, int h): width {w}, height {h} {}
+		Rectangle operator+ (const Rectangle & other);
 		double area();
 		double perimeter();
 
 };
 
+// Overloading + operator to add two rectangles. widths and heights are added and 
+// A new Rectangle object return.
+Rectangle Rectangle::operator+(const Rectangle &other)
+{
+	Rectangle temp;
+	
+	temp.width = width + other.width;
+	temp.height = height + other.height;
+	
+	return temp;
+}
 
 double Rectangle::area()
 {
@@ -27,6 +39,7 @@ int main()
 {
 	Rectangle rectangleA {3,5};
 	Rectangle rectangleB; // Uses default constructor
+	Rectangle rectangleC;
 
 	std::cout << "Rectangle A area: " << rectangleA.area() << '\n';
 	std::cout << "Rectangle A perimeter: " << rectangleA.perimeter() << '\n';
@@ -34,5 +47,10 @@ int main()
 	std::cout << "Rectangle B area: " << rectangleB.area() << '\n';
 	std::cout << "Rectangle B perimeter: " << rectangleB.perimeter() << '\n';
 
+	std::cout << "----Demonstrating operator+ overloading----\n";
+	rectangleC =  rectangleA + rectangleB;
+	std::cout << "Area (rectangleA (3,5) + rectangleB (1,1)): " << rectangleC.area() << '\n';
+	std::cout << "Perimeter (rectangleA (3,5) + rectangleB (1,1)): " << rectangleC.perimeter() << '\n';
+	
 	return 0;
 }
