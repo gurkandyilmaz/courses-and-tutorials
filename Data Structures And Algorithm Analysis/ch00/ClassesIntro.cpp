dd9b383aa1ae9fab6b143e1e817fc626d0dbc18a
Gurkan Yilmaz <gurkandy@gmail.com>
2021-05-01 23:29:16 +0300

    constructors added

--- Data Structures And Algorithm Analysis/ch00/ClassesIntro.cpp
+++ Data Structures And Algorithm Analysis/ch00/ClassesIntro.cpp
@@ -3,35 +3,36 @@
 #include <iostream>
 
 class Rectangle {
-	int width;
-	int height;
+		int width, height; // private members.
 	public:
-	void set_sides(int x, int y);
-	double area();
+		Rectangle(): width {1}, height {1}{} // default constructor sets width and height to 1.
+		Rectangle(int w, int h): width {w}, height {h} {}
+		double area();
+		double perimeter();
 
 };
 
-void Rectangle::set_sides(int w, int h)
-{
-	width = w;
-	height = h;
-	std::cout << "Width: " << width << ", Height: " << height << "\n";
-}
 
 double Rectangle::area()
 {
 	return width*height;
 }
 
+double Rectangle::perimeter()
+{
+	return 2*(height + width);
+}
+
 int main()
 {
-	Rectangle rectangleA, rectangleB;
+	Rectangle rectangleA {3,5};
+	Rectangle rectangleB; // Uses default constructor
 
-	rectangleA.set_sides(3,5);
-	std::cout << "Rectangle A area: " << rectangleA.area() << "\n";
+	std::cout << "Rectangle A area: " << rectangleA.area() << '\n';
+	std::cout << "Rectangle A perimeter: " << rectangleA.perimeter() << '\n';
 
-	rectangleB.set_sides(4,8);
-	std::cout << "Rectangle B area: " << rectangleB.area() << "\n";
-	
+	std::cout << "Rectangle B area: " << rectangleB.area() << '\n';
+	std::cout << "Rectangle B perimeter: " << rectangleB.perimeter() << '\n';
+
 	return 0;
 }
