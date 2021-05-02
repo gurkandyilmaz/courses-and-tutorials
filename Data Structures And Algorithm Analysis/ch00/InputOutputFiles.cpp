0408f278dfb6d82f1b169a05f335da3c370854ba
Gurkan Yilmaz <gurkandy@gmail.com>
2021-05-02 23:43:37 +0300

    read content from a file

--- Data Structures And Algorithm Analysis/ch00/InputOutputFiles.cpp
+++ Data Structures And Algorithm Analysis/ch00/InputOutputFiles.cpp
@@ -10,8 +10,10 @@ void read_from(std::string file_name);
 int main()
 {
 	std::string message = "This is an example text to be written.\n";
+	std::string file = "InputOutputFiles.txt";
 
-	write_to("InputOutputFile.txt", message);
+	write_to(file, message);
+	read_from(file);
 	return 0;
 }
 
@@ -23,3 +25,23 @@ void write_to(std::string filename, std::string &content)
 	myfile.close();
 }
 
+void read_from(std::string filename)
+{
+	std::ifstream myfile (filename);
+	std::string content;
+	std::string line;
+	
+	if (myfile.is_open())
+	{
+		while(getline(myfile, line))
+		{
+			content += line;
+		}
+	}
+	else
+	{
+		std::cout << "File could not be read.\n";
+	}
+
+	std::cout << "The file content: " << content << '\n';
+}
