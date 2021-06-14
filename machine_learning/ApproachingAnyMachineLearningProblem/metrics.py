531093bb6d7776272aaae3686a4d4bcceed6d21b
Gurkan Yilmaz <gurkandy@gmail.com>
2021-06-15 00:42:03 +0300

    log_loss

--- machine_learning/ApproachingAnyMachineLearningProblem/metrics.py
+++ machine_learning/ApproachingAnyMachineLearningProblem/metrics.py
@@ -1,7 +1,7 @@
 """ Metrics for classification and regression tasks """
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn import metrics
 
 def accuracy(y_true, y_pred):
     """ If the data is skewed accuracy may not be the best option."""
@@ -89,6 +89,26 @@ def f1(y_true, y_pred):
     f1 = 2*tp / (2*tp + fp + fn)
     return f1
 
+def log_loss(y_true, y_pred_proba):
+    """Compute log loss. It penalizes predictions that are too high or low.
+        Log loss = -1*(target*log(prediction) + (1-target)*log(1-prediction))    
+    """
+    epsilon = 1e-15 # clip predictions below epsilon and above 1-epsilon
+    losses = []
+    for y_t, y_p in zip(y_true, y_pred_proba):
+        y_p = np.clip(y_p, epsilon, 1 - epsilon)
+        loss = -1.0 * (y_t * np.log(y_p) + (1 - y_t) * np.log(1 - y_p))
+        losses.append(loss)
+    
+    print(losses)
+    plt.figure(figsize=(12,8))
+    plt.scatter(y_pred_proba, losses)
+    plt.xlabel("proba")
+    plt.ylabel("losses")
+    plt.show()
+    return np.mean(losses)
+
+# Helpers BEGIN
 def apply_threshold(array, threshold = 0.5):
     """
         Convert the values in the array either to 0 or 1 depending on the threshold. 
@@ -128,28 +148,24 @@ def plot_precision_recall(y_true, y_pred_prob):
     plt.xlabel("Recall")
     plt.show()
 
+# Helpers END
+
 if __name__ == "__main__":
     y_true = [1,0,1,1,0,0,0,0,1,0,1,0,0,1,0,1,0,1,1,0]
     y_pred = [1,0,1,0,1,0,1,1,0,1,1,0,0,1,0,1,1,0,1,0]
-    # y_pred for plot_precision_recall()
-#    y_pred = [
-#        0.263, 0.111, 0.416, 0.147, 0.119, 0.175,
-#        0.159, 0.838, 0.116, 0.177, 0.085, 0.390,
-#        0.272, 0.034, 0.046, 0.034, 0.185, 0.059,
-#        0.619, 0.330
-#    ]
-
+    y_pred_proba = [0.1, 0.98, 0.9, 0.15, 0.33, 0.42, 0.5, 0.16, 0.8, 0.7, 
+                    0.8, 0.25, 0.77, 0.4, 0.6, 0.3, 0.56, 0.85, 0.97, 0.22]
 #    print("Custom Accuracy: ", accuracy(y_true, y_pred))
 #    print("Sklearn Accuracy: ", accuracy_score(y_true, y_pred))
 #    print("Accuracy V2: ", accuracy_v2(y_true, y_pred))
 #    print("----------------------")
     print("Custom Precision: ", precision(y_true, y_pred))
-    print("Sklearn Precision: ", precision_score(y_true, y_pred))
+    print("Sklearn Precision: ", metrics.precision_score(y_true, y_pred))
     print("Custom Recall: ", recall(y_true, y_pred))
-    print("Sklearn Recall: ", recall_score(y_true, y_pred))
+    print("Sklearn Recall: ", metrics.recall_score(y_true, y_pred))
 #    plot_precision_recall(y_true, y_pred)
     print("Custom F1: ", f1(y_true, y_pred))
-    print("Sklearn F1: ", f1_score(y_true, y_pred))
-
-
+    print("Sklearn F1: ", metrics.f1_score(y_true, y_pred))
+    print("Custom Log loss: ", log_loss(y_true, y_pred_proba))
+    print("Sklearn Log loss: ", metrics.log_loss(y_true, y_pred_proba))
 
