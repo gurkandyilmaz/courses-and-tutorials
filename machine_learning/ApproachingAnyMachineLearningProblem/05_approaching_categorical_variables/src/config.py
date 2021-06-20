dae4c2d93f7261be259c7676d1489bb829b1a4b6
Gurkan Yilmaz <gurkandy@gmail.com>
2021-06-20 16:34:48 +0300

    create folds

--- machine_learning/ApproachingAnyMachineLearningProblem/05_approaching_categorical_variables/src/config.py
+++ machine_learning/ApproachingAnyMachineLearningProblem/05_approaching_categorical_variables/src/config.py
@@ -4,3 +4,5 @@ PROJECT_DIR = Path.cwd().parent
 INPUT_DIR = PROJECT_DIR / "input/"
 MODEL_DIR = PROJECT_DIR / "models/"
 
+TRAIN_FILE = INPUT_DIR / "categorical_features_train_folds.csv"
+TEST_FILE = INPUT_DIR / "categorical_features_test.csv"
---
+++ machine_learning/ApproachingAnyMachineLearningProblem/05_approaching_categorical_variables/src/create_folds.py
@@ -0,0 +1,23 @@
+"""This file is used to take the input data then assign random folds to them based on the target distribution."""
+
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+
+import config
+
+def create_folds(input_file, target = "target", num_folds = 5):
+    df = pd.read_csv(config.INPUT_DIR / input_file, index_col="id")
+    
+    df["folds"] = None
+    kf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=12)
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df[target])):
+        df.loc[val_idx, "folds"] = fold
+    
+    input_file_folds = input_file.split(".")[0]
+    input_file_folds = f"{input_file_folds}_folds.csv"
+
+    df.to_csv(config.INPUT_DIR / input_file_folds, index=False)
+
+if __name__ == "__main__":
+    train_file = "categorical_features_train.csv"
+    create_folds(train_file)
