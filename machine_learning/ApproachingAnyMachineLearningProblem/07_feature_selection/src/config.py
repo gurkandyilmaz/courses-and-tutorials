0eebccb808f790c6b625d1a5517a1967912344b4
Gurkan Yilmaz <gurkandy@gmail.com>
2021-06-22 01:40:37 +0300

    make random classification, regression data

---
+++ machine_learning/ApproachingAnyMachineLearningProblem/07_feature_selection/src/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_DIR = Path.cwd().parent
+INPUT_DIR = PROJECT_DIR / "input"
+
+RANDOM_CLASSIFICATION_DATA = INPUT_DIR / "random_classification_data.csv"
+RANDOM_REGRESSION_DATA = INPUT_DIR / "random_regression_data.csv"
