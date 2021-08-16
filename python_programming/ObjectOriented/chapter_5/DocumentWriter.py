f7fb60ce595ded3fde887f798a0a249b2f8ae1d6
GurkanD Yilmaz <gurkandy@gmail.com>
2021-08-17 00:05:38 +0300

    document methods

--- python_programming/ObjectOriented/chapter_5/DocumentWriter.py
+++ python_programming/ObjectOriented/chapter_5/DocumentWriter.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 
 
+class Document:
+    def __init__(self, filename: str):
+        self.filename = filename
 
+    def insert(self, character):
+        pass
 
+    def delete(self, character):
+        pass
 
+    def forward(self):
+        pass
 
+    def back(self):
+        pass
 
 
 
 
 if __name__ == "__main__":
-    print(__doc__)
-    print(__file__)
-    print(Path(__file__).absolute())
-    print(Path(__file__).stat())
-    print(Path(__file__).resolve())
     print(Path(__file__).cwd())
     print(Path(__file__).parent)
-
