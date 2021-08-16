29a90e03ac0785a346caa1a0a850cf052e58e1a3
GurkanD Yilmaz <gurkandy@gmail.com>
2021-08-17 00:22:51 +0300

    Document methods implemented

--- python_programming/ObjectOriented/chapter_5/DocumentWriter.py
+++ python_programming/ObjectOriented/chapter_5/DocumentWriter.py
@@ -4,22 +4,46 @@ from pathlib import Path
 class Document:
     def __init__(self, filename: str):
         self.filename = filename
+        self._characters = list()
+        self._cursor = 0
+        self._line_length = 10
 
     def insert(self, character):
-        pass
+        if len(character) == 1:
+            self._characters.insert(self._cursor, character)
+            self._cursor += 1
+        else:
+            raise ValueError("Only single characters are allowed.")
 
-    def delete(self, character):
+    def delete(self):
         pass
 
     def forward(self):
-        pass
+        if self._cursor < self._line_length:
+            self._cursor += 1
+        else:
+            raise EOFError("End od line Error.")
 
     def back(self):
-        pass
+        self._cursor -= 1
+
+    def __str__(self):
+        print("Cursor at: ", self._cursor)
+        return "".join(self._characters)
 
 
 
 
 if __name__ == "__main__":
-    print(Path(__file__).cwd())
-    print(Path(__file__).parent)
+    document = Document("test.txt")
+    document.insert("t")
+    document.insert("h")
+    document.insert("i")
+    document.insert("s")
+    document.back()
+    document.back()
+    document.insert("x")
+
+    print(document)
+
+
