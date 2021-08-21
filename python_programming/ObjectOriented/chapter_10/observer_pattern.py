fa510b14c4692350b0b5ae1f466399320d110ae6
GurkanD Yilmaz <gurkandy@gmail.com>
2021-08-21 22:46:28 +0300

    decorator and observer pattern examples

---
+++ python_programming/ObjectOriented/chapter_10/observer_pattern.py
@@ -0,0 +1,58 @@
+"""Observer pattern example."""
+
+class BankAccount:
+    """
+    This class is the observed class.Name and Balance attributes will be 
+    observed by other class, namely Observer. Everytime the two attributes
+    are updated observers will see the updated values.
+    """
+    def __init__(self) -> None:
+        self.observers = []
+        self._name = None
+        self._balance = None
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = name
+        self._update_observers()
+
+    @property
+    def balance(self) -> int:
+        return self._balance
+
+    @balance.setter
+    def balance(self, balance: int) -> None:
+        self._balance = balance
+        self._update_observers()
+    
+    def _update_observers(self) -> None:
+        for observer in self.observers:
+            observer()
+    
+class Observer:
+    """Observers will be updated if there is a change in the BankAccount"""
+    count = 0
+    def __init__(self, account: BankAccount) -> None:
+        self.account = account
+        self.account.observers.append(self)
+        self.observer_id = Observer.count + 1
+        Observer.count += 1
+
+    def __call__(self):
+        print(f"Dear Observer {self.observer_id}, BankAccount updated!")
+        print(f"Name: {self.account.name} | Balance:{self.account.balance}")
+        print("--------------------")
+
+if __name__ == "__main__":
+    account = BankAccount()
+    observer_1 = Observer(account)
+    account.name = "ING-Yenice-Vadeli"
+    observer_2 = Observer(account)
+    observer_3 = Observer(account)
+    #account.name = "Test name"
+    account.balance = 1200
+
