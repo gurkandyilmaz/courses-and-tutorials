9881e131ec2b884e1dfe231cee0836344d545c9c
GurkanD Yilmaz <gurkandy@gmail.com>
2021-08-21 16:34:24 +0300

    coroutines examples implemented

--- python_programming/ObjectOriented/chapter_9/coroutines.py
+++ python_programming/ObjectOriented/chapter_9/coroutines.py
@@ -1,4 +1,19 @@
 """This demonstrates one of the use of coroutines."""
+import re
+from typing import Any
+
+def match_regex(filename: str, pattern: str) -> Any:
+    print("Başla...")
+    with open(filename) as file:
+        lines = file.readlines()
+    for line in reversed(lines):
+        match = re.match(pattern, line)
+        print("MATCH: ", match)
+        if match:
+            print("GROUPS: ", match.groups())
+            print("AAAA")
+            regex = yield match.groups()[0]
+            print("REGEX: ", regex)
 
 def incrementer():
     score = 0
@@ -6,13 +21,15 @@ def incrementer():
         increment = yield score
         score += increment
 
-
 if __name__ == "__main__":
     first = incrementer()
     second = incrementer()
+#    print(next(first))
+#    print(next(second))
+#    print(first.send(3))
+#    print(second.send(1))
+    ERROR_PATTERN = r"XFS ERROR (\[sd[a-z]\])"
+    matcher = match_regex("some.logs", ERROR_PATTERN)
+    print("NEXT: ", next(matcher))
+    print("SEND: ", matcher.send("#"))
 
-    print(next(first))
-    print(next(second))
-
-    print(first.send(3))
-    print(second.send(1))
