c0234d062a74a333c975e8be2f945087dafb44b2
GurkanD Yilmaz <gurkandy@gmail.com>
2021-09-13 23:55:25 +0300

    created index page and updated readme

--- machine_learning/HandsOnDLforWeb/mnist_and_flask/README.md
+++ machine_learning/HandsOnDLforWeb/mnist_and_flask/README.md
@@ -1,11 +1,21 @@
-A simple flask app that serves a neural network trained on MNIST dataset.
-The app can be used via its APIs or from the UI.
+**A simple flask app that serves a cnn network trained on MNIST dataset.
+The app can be used via its APIs or from the UI.**
 
-Run the app by typing the following command in the terminal
+- Creating a virtual environment is optional but recommended. Use either pip or conda:
+```
+HandsOnDLforWeb $: conda create --name any_name_for_environment --file=conda_requirements.txt 
+```
+- Run the app by typing the following command in the terminal:
 
 ```python
-python mnsit_flask.py
+HandsOnDLforWeb $: python flask_api.py
 ```
+- Go to the localhost in your browser:
+```
+http://127.0.0.1:5000
+```
+
+An index page will be displayed and you can upload your mnist (8x8) image for prediction.
 
 
 
--- machine_learning/HandsOnDLforWeb/mnist_and_flask/flask_api.py
+++ machine_learning/HandsOnDLforWeb/mnist_and_flask/flask_api.py
@@ -9,18 +9,19 @@ import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 
 from config import MNIST, APP
+from custom_logger import get_logger
 from prepate_data import loadImageFile, loadLabelFile, generate_images
 from utils import stringToImage, load_model
 
+LOGGER = get_logger(__name__)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'TODO an index page'
+    return render_template('index.html')
 
 @app.route('/predict/', methods=['POST'])
 def predict():
-    # TODO add a logger for api
     data = request.files['img']
     img = Image.open(data.stream)
     img.load()
@@ -28,21 +29,28 @@ def predict():
     image.paste(img, mask=img.split()[3])
     image = np.array(image)
     image = np.reshape(image, (1,28,28,1)) / 255
+    LOGGER.info(f'Image loaded having format and shape as: {type(image)} - {image.shape}')
     
     graph = tf.compat.v1.get_default_graph()
     with graph.as_default():
         with open(MNIST.MODEL_JSON) as f:
             model = model_from_json(f.read())
+        LOGGER.info(f'Model loaded: {model}')
         model.load_weights(str(MNIST.MODEL_WEIGHTS))
         model.compile(
                 loss='categorical_crossentropy',
                 optimizer='adam',
                 metrics=['accuracy'],
         )
+        LOGGER.debug(f'Model compiled: {model.get_config()}')
         pred = model.predict(image)
+        label = np.argmax(pred, axis=1)[0]
+        LOGGER.debug(f'Prediction was made: {pred}')
         pred = [f'{float(score):.9f}' for score in pred[0]]
         pred = {str(idx):score for idx, score in enumerate(pred)}
-    return jsonify({'response': pred})
+        response = jsonify({'response': pred})
+        LOGGER.debug(f'API response: {response.data}')
+    return str(label)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
